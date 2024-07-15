e2533198e26b6b6f1687c0172d6419c28341946c
mathdsousa <matheus.d.sousall@gmail.com>
2024-07-15 16:58:00 -0300

    Update domain Profissional

--- SistemaDeVagas/src/main/java/br/ufscar/dc/dsw/domain/Profissional.java
+++ SistemaDeVagas/src/main/java/br/ufscar/dc/dsw/domain/Profissional.java
@@ -20,4 +20,44 @@ public class Profissional {
         this.telefone = telefone;
         this.usuario = usuario;
     }
+
+    public String getCpf(){
+        return cpf;
+    }
+
+    public void setCPF(String cpf){
+        this.cpf = cpf;
+    }
+
+    public String getData_nasc(){
+        return data_nasc;
+    }
+
+    public void setData_nasc(String data_nasc){
+        this.data_nasc = data_nasc;
+    }
+
+    public String getSexo(){
+        return sexo;
+    }
+
+    public void setSexo(String sexo){
+        this.sexo = sexo;
+    }
+
+    public String getTelefone(){
+        return telefone;
+    }
+
+    public void setTelefone(String telefone){
+        this.telefone = telefone;
+    }
+
+    public Usuario getUsuario(){
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario){
+        this.usuario = usuario;
+    }
 }
