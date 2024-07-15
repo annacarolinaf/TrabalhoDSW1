d2fadccb4e46becb3b99a92f594a9cf9eb3eb1e0
mathdsousa <matheus.d.sousall@gmail.com>
2024-07-15 16:51:19 -0300

    update domain Profissional

--- SistemaDeVagas/src/main/java/br/ufscar/dc/dsw/domain/Profissional.java
+++ SistemaDeVagas/src/main/java/br/ufscar/dc/dsw/domain/Profissional.java
@@ -1,18 +1,23 @@
 package br.ufscar.dc.dsw.domain;
 
 public class Profissional {
-//     create table Profissional(
-//     id_prof bigint not null auto_increment, 
-//     cnpj varchar(18) not null unique,
-//     nome varchar(256) not null,
-//     email varchar(256) not null unique,
-//     senha varchar(256) not null,
-//     data_nasc date not null,
-//     sexo varchar(256) not null,
-//     telefone varchar(500) not null,
-//     primary key (id_prof)
-//     );
 
-    
+    private String cpf;
+    private String data_nasc;
+    private String sexo;
+    private String telefone;
+    private Usuario usuario;
 
+    public Profissional(String cpf){
+        this.cpf = cpf;
+    }
+
+    public Profissional(String cpf, String data_nasc, String sexo, String telefone, Usuario usuario)
+    {
+        this(cpf);
+        this.data_nasc = data_nasc;
+        this.sexo = sexo;
+        this.telefone = telefone;
+        this.usuario = usuario;
+    }
 }
