d58a45bd075df217fb20068ff434cef79d8ab6d9
Karys Barbosa <karys.barbosa@outlook.com>
2024-07-15 17:07:28 -0300

    Vaga adicionado
    Colocado via github site

---
+++ SistemaDeVagas/src/main/java/br/ufscar/dc/dsw/domain/Vaga.java
@@ -0,0 +1,67 @@
+package br.ufscar.dc.dsw.domain;
+
+public class Vaga {
+
+    private Long id_vaga;
+    private float salario;
+    private String descricao;
+    private String data_limite;
+    private Empresa empresa;
+
+    public Vaga(Long vaga) {
+        this.id_vaga = id_vaga;
+    }
+
+
+    public Vaga( float salario, String descricao, String data_limite, Empresa empresa) {
+        this.salario = salario;
+        this.descricao = descricao;
+        this.data_limite = data_limite;
+        this.empresa = empresa;
+    }
+    
+    public Vaga( Long id_vaga,  float salario, String descricao, String data_limite, Empresa empresa) {
+        this(salario, descricao, data_limite, empresa);
+        this.id_vaga = id_vaga;
+    }
+
+    public Long getId_vaga() {
+        return id;
+    }
+
+    public void setId_vaga(Long id_vaga) {
+        this.id_vaga = id_vaga;
+    }
+
+    public Float getSalario() {
+        return salario;
+    }
+
+    public void setSalario(Float salario) {
+        this.salario = salario;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getData_limite() {
+        return data_limite;
+    }
+
+    public void setData_limite(String data_limite) {
+        this.data_limite = data_limite;
+    }
+
+    public Empresa getEmpresa() {
+        return empresa;
+    }
+
+    public void setEmpresa(Empresaa empresa) {
+        this.empresa = empresa;
+    }
+}
