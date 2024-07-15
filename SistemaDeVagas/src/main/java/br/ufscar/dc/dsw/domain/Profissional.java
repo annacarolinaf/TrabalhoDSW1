87cbb2b67d62048ae987d19d767d5c8567d97bbb
mathdsousa <matheus.d.sousall@gmail.com>
2024-07-15 16:39:17 -0300

    update Usuario e Empresa

---
+++ SistemaDeVagas/src/main/java/br/ufscar/dc/dsw/domain/Profissional.java
@@ -0,0 +1,18 @@
+package br.ufscar.dc.dsw.domain;
+
+public class Profissional {
+//     create table Profissional(
+//     id_prof bigint not null auto_increment, 
+//     cnpj varchar(18) not null unique,
+//     nome varchar(256) not null,
+//     email varchar(256) not null unique,
+//     senha varchar(256) not null,
+//     data_nasc date not null,
+//     sexo varchar(256) not null,
+//     telefone varchar(500) not null,
+//     primary key (id_prof)
+//     );
+
+    
+
+}
